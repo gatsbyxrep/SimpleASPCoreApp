691a15b6d96744fe3ab59f519418e45b7686a088
russiancplusplus <gatsbynalda@gmail.com>
2021-02-09 19:44:29 +0300

    Init

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from movie in _context.Movie select movie;
+
+            if(SearchString != null && SearchString != "")
+                movies = movies.Where(movie => movie.Title.Contains(SearchString));
+
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
